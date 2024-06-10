e7a1de2d47370d43eb9f7ba7791099db3dce2ef3
Datta997 <111185883+Datta997@users.noreply.github.com>
2024-06-10 07:51:01 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // this use strict will treat your JS code at its newer version 
+//but now in modern js your entire code automatically trated it as newer code 
+
+
+let name = "Datta";
+let age = 18;
+let isLoggedIn = true;
+
+//number => 2 to power of 53
+// bigint 
+// string => "" or ''
+//boolean => true/false 
+// null => standAlone value it is a type *defined but holding empty value
+// undefined => when you not defined value veriable declare but not defined
+// symbol =>unique ( most time we are going to use in in reactsJS )
+// object 
+
+
+console.log(typeof isLoggedIn);
