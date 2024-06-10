2954cef4ee73fc425c4ea12cf320e98de1366fde
Datta997 <111185883+Datta997@users.noreply.github.com>
2024-06-10 07:51:59 +0000

    veriables

---
+++ .devcontainer/01_basics/01_veriables.js
@@ -0,0 +1,14 @@
+const accoundId = 1443344;
+let accountName= "Datta Deokhale";
+var accountMail = "dd@gmail.com";
+adress = "naregaon SambhajiNagar"
+
+console.log(accoundId);
+console.table([accoundId, accountName, accountMail, adress]);
+
+/*
+due to scope issue try to avoid using var keyword 
+const - when you want to make variable constant value shpuld not get changed even after it try to change anywhere in code
+let - scope basis
+*/
+
